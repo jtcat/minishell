2cbfea94d27a5ac3280e4b4ee6d46287d5994db4
jcat <jftcatarino@gmail.com>
2023-04-25 12:05:43 +0100

    move Minishell.h to include dir

--- main.c
+++ main.c
@@ -6,14 +6,24 @@
 /*   By: leborges <leborges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:30:28 by leborges          #+#    #+#             */
-/*   Updated: 2023/04/24 17:33:06 by leborges         ###   ########.fr       */
+/*   Updated: 2023/04/25 11:27:54 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Minishell.h"
+#include "libft.h"
+#include <readline/history.h>
+#include <readline/readline.h>
 
 int	main()
 {
-	char *input;
+	char	*input;
+
 	input = readline("Super mini shellings$ ");
+	while (input)
+	{
+		input = readline("Super mini shellings$ ");
+		add_history(input);
+		rl_redisplay();
+	}
 }
