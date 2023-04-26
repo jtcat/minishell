d5a649a37ff215c61e39209c2247ba8203e65906
jcat <jftcatarino@gmail.com>
2023-04-27 00:33:08 +0100

    fix norm, add provisory cmd struct

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,14 @@
 /*   By: leborges <leborges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:30:25 by leborges          #+#    #+#             */
-/*   Updated: 2023/04/26 21:01:52 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/04/27 00:17:22 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stddef.h>
+# include <stddef.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -21,30 +21,38 @@
 # include <readline/history.h>
 # include <libft.h>
 
-enum token_type {NEW_LINE, NANE, OPERATOR, WORD, TOKEN};
-typedef enum token_type token_type;
+# define PROMPT "minishell >"
 
-// Lexer
-t_list	*split_tokens(char *str);
+enum e_token_type {new_line, name, operator, word, token};
+typedef enum e_token_type	t_token_type;
 
 typedef struct s_token
 {
 	char			*string;
-	enum token_type	type;
+	t_token_type	type;
 }			t_token;
-
-# define PROMPT "minishell >"
 
 typedef struct s_scontext
 {
-	char const 	**envp;
-	char const 	**svars;
-	size_t	envp_len;
+	char const	**envp;
+	char const	**svars;
+	size_t		envp_len;
 }	t_scontext;
 
+typedef struct s_cmd
+{
+	char const	*cmd_name;
+	char const	*args;
+	char const	**exports;
+	char const	*red_in;
+	char const	*red_out;
+}	t_cmd;
+
+// Lexer
+t_list	*split_tokens(char *str);
+
 // Builtins
-int	pwd_cmd(void);
-int env_cmd(t_scontext *ctx);
-int	echo_cmd(char *str[], int opt_n);
-	
+int		pwd_cmd(void);
+int		env_cmd(t_scontext *ctx);
+int		echo_cmd(char *str[], int opt_n);
 #endif
