cdd934279764e41f2ded094994b800f2ba4d6aa8
jcat <jftcatarino@gmail.com>
2023-04-26 20:56:42 +0100

    test builtins

--- main.c
+++ main.c
@@ -6,16 +6,18 @@
 /*   By: leborges <leborges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:30:28 by leborges          #+#    #+#             */
-/*   Updated: 2023/04/25 21:02:53 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/04/26 20:56:31 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <Minishell.h>
+#include <minishell.h>
 
-int	main(int argc, char *argv[], char *envp[])
+int	main(int argc, char *argv[], char const *envp[])
 {
 	char	*input;
+	t_scontext	ctx;
 
+	ctx.envp = envp;
 	(void)argv;
 	if (argc == 1)
 	{
@@ -29,8 +31,11 @@ int	main(int argc, char *argv[], char *envp[])
 			free(input);
 			input = readline(PROMPT);
 		}
-		env(envp);
-		pwd();
+		env_cmd(&ctx);
+		pwd_cmd();
+		echo_cmd((char *[]){"ATMD4\n", NULL}, 0);
+		echo_cmd((char *[]){"ATMD4SEMESPACO\n", "okboafa", NULL}, 1);
+		echo_cmd((char *[]){"ATMD4\n", NULL}, 0);
 		rl_clear_history();
 	}
 }
