f5a4a82ec96e56c2a294c0c3db951cd5fbed6258
Leonardo Filipe Borges Silva <leborges@c1r15s3.42porto.com>
2023-04-26 20:26:25 +0100

    test lexer

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: leborges <leborges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:30:28 by leborges          #+#    #+#             */
-/*   Updated: 2023/04/25 12:52:51 by leborges         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:13:48 by leborges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@
 int	main()
 {
 	char	*input;
+	t_list	*list;
 
 	input = readline("Super mini shellings$ ");
-	while (input)
+	list = split_tokens(input);
+
+	while (list)
+	{
+		printf("Data: %s | Token: %u\n", ((t_token *)list->content)->string, ((t_token *)list->content)->type);
+		list = list->next;
+	}
+
+	/*while (input)
 	{
 		input = readline("Super mini shellings$ ");
 		add_history(input);
 		rl_redisplay();
-	}
+	}*/
 }
