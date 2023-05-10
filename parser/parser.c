d96e8a9930e7443f273b63efa1acb3d8ec6506c9
Joao Francisco <joaoteix@student.42porto.com>
2023-05-10 17:43:43 +0100

    idk

--- parser/parser.c
+++ parser/parser.c
@@ -6,51 +6,57 @@
 /*   By: joaoteix <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:15:56 by joaoteix          #+#    #+#             */
-/*   Updated: 2023/05/05 21:07:24 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/05/10 13:18:40 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ * Command grammar:
+ *
+ * simple_cmd: simple_cmd ( '|' simple_cmd)*
+ * 			| assigment* (redirect | word)*
+ * 			| assignment
+ * 			| (redirect | word)
+ * 			;
+ */
+
 int	is_ctrl_op(t_token token)
 {
-	return (!token && token->type == pipe);
+	return (token && token->type == pipe);
 }
 
-void	parse_cmd(t_list *tokens, t_list *cmd_list)
+int	parse_cmd(t_list *tokens, t_cmd *cmd, int pipe_out[2])
 {
-	t_cmd	*cmd;
-
-	cmd = new_cmd();
-	cmd->pipe_in == cmd_list->content->pipe_out
-	while (!is_ctrl_op(tokens->content) && ft_strchar(tokens->content->str, '='))
+	cmd->pipe_in == pipe_out;
+	while (*tokens && strchar((*tokens)->content->str, '='));
 	{
-		ft_lstadd_back(cmd->exports, ft_lstnew(tokens->content));
-		tokens = tokens->next;
+		ft_lstadd_back(cmd->exports, ft_lstnew((*tokens)->content));
+		*tokens = (*tokens)->next;
 	}
-	while (!is_ctrl_op(tokens->content))
+	while (!is_ctrl_op((*tokens)->content))
 	{
 		if (tokens->content->type == red_in)
-			cmd->red_in = tokens->next->content->str;
-		else if (tokens->content->type == red_out)
-			cmd->red_out = tokens->next->content->str;
-		else if (tokens->content->type == here_doc)
-			cmd->hd_delimit = tokens->next->content->str;
+			cmd->red_in = (*tokens)->next->content->str;
+		else if ((*tokens)->content->type == red_out)
+			cmd->red_out = (*tokens)->next->content->str;
+		else if ((*tokens)->content->type == here_doc)
+			cmd->hd_delimit = (*tokens)->next->content->str;
+		else if ((*tokens)->content->type == word)
+			ft_lstadd_back(cmd->args, ft_lstnew((*tokens)->content->str));
 		else
-			ft_lstadd_back(cmd->args, ft_lstnew(tokens->content->str));
-		if (tokens->content->type == red_in || tokens->content->type == red_out
-				|| tokens->content->type == red_out
-				|| tokens->content->type == here_doc)
-			tokens = tokens->next;
-		tokens = tokens->next;
+			return (0);
+		if ((*tokens)->content->type == red_in || (*tokens)->content->type == red_out
+				|| (*tokens)->content->type == here_doc)
+			*tokens = (*tokens)->next;
+		*tokens = (*tokens)->next;
 	}
-	ft_lstadd_back(cmd_list, cmd);
+	return (1);
 }
 
 void	parse_pipeline(t_list *tokens, t_list *cmds)
 {
 	while (tokens)
 	{
-		if (tokens->content->type == pipe)
-			cmds->pipe_out = 1;
 	}
 }
 
@@ -59,5 +65,12 @@ t_list	*parse_tokens(t_list	*tokens_head)
 	t_list	*cmd_list;
 
 	cmd_list = NULL;
+	while (tokens)
+	{
+		ft_lstadd_back(cmd_list, new_cmd());
+		parse_cmd(&tokens, cmd_list->content);
+		if (token->type == pipe)
+					
+	}
 	return (cmd_list);
 }
