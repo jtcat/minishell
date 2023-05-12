ff41d846b7e5f64aa4bb6d96975545aec962cd84
jcat <jftcatarino@gmail.com>
2023-05-13 00:09:33 +0100

    add token char matching functions

--- utils/str_utils.c
+++ utils/str_utils.c
@@ -6,7 +6,7 @@
 /*   By: joaoteix <joaoteix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 23:33:09 by joaoteix          #+#    #+#             */
-/*   Updated: 2023/04/26 23:57:16 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/05/12 22:32:19 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,14 @@ int	is_blank(char c)
 	return (c == ' ' || c == '\t');
 }
 
-int	is_quote(char c)
-{
-	return (c == '"' || c == '\'');
-}
-
 int	is_op(char c)
 {
-	return (c == '>' || c == '<' || c == '|');
+	return (c == '&' || c == '>' || c == '<' || c == '|');
 }
 
-int	is_delim(char c)
+int	is_wordchar(char c)
 {
-	return (c == '\0' || is_blank(c) || is_op(c));
+	return (c != '\0' && !is_blank(c) && !is_op(c));
 }
 
 int	is_blank_str(char str[])
