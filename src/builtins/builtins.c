039a84219326346373c38edee1bc711ebfdc3762
Joao Francisco <joaoteix@student.42porto.com>
2023-10-29 18:29:34 +0000

    fix norm

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: leborges <leborges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 14:22:48 by joaoteix          #+#    #+#             */
-/*   Updated: 2023/10/29 17:19:57 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/10/29 18:05:13 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@
 // Return values represent exit status
 
 int	echo_cmd(t_shctx *ctx, char **args)
-{	
+{
 	int	n_opt;
-	(void)ctx;
 
+	(void)ctx;
 	n_opt = *args && (ft_strcmp(args[0], "-n") == 0);
 	if (n_opt)
 		args++;
@@ -69,18 +69,19 @@ void	set_var(t_shctx *ctx, const char *var, const char *val)
 
 int	cd_cmd(t_shctx *ctx, char **args)
 {
-	char	*curpath;
-	char	*pwd = get_var_val(ctx, "PWD");
-	char	*home = get_var_val(ctx, "HOME");
+	char		*curpath;
+	char const	*pwd = get_var_val(ctx, "PWD");
+	char const	*home = get_var_val(ctx, "HOME");
 
 	if (*(args + 1) != NULL)
 		ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX "cd: too many arguments\n");
 	return (ERRC_CD_ARGS);
 	if (!*args && *home)
-		curpath = home;
+		curpath = (char *)home;
 	if (chdir(curpath) != 0)
 	{
-		ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX "cd: %s: %s\n", *args, strerror(errno));
+		ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX "cd: %s: %s\n",
+			*args, strerror(errno));
 		return (1);
 	}
 	set_var(ctx, "PWD", curpath);
@@ -91,9 +92,9 @@ int	cd_cmd(t_shctx *ctx, char **args)
 int	pwd_cmd(t_shctx *ctx, char **args)
 {
 	char	*cwd;
+
 	(void)ctx;
 	(void)args;
-
 	cwd = getcwd(NULL, 0);
 	ft_dprintf(STDOUT_FILENO, "%s\n", cwd);
 	free(cwd);
@@ -102,9 +103,10 @@ int	pwd_cmd(t_shctx *ctx, char **args)
 
 int	env_cmd(t_shctx *ctx, char **args)
 {
-	t_dlist	*iter = ctx->envp;
-	(void)args;
+	t_dlist	*iter;
 
+	iter = ctx->envp;
+	(void)args;
 	while (iter)
 	{
 		if (ft_strchr(iter->content, '='))
@@ -113,6 +115,9 @@ int	env_cmd(t_shctx *ctx, char **args)
 	}
 	return (0);
 }
+
+#define ERR_EXIT_NAN "exit: non numeric argument: %s\n"
+#define ERR_EXIT_ARGC "exit: too many arguments\n"
 
 int	exit_cmd(t_shctx *ctx, char **args)
 {
@@ -124,12 +129,12 @@ int	exit_cmd(t_shctx *ctx, char **args)
 		exit_code = ft_atoi(*args, &is_num);
 		if (!is_num)
 		{
-			ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX "exit: non numeric argument: %s\n", *args);
+			ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX ERR_EXIT_NAN, *args);
 			ctx->cmd_status = 2;
 		}
 		else if (*(args + 1) != NULL)
 		{
-			ft_putstr_fd(MSH_ERR_PFIX "exit: too many arguments\n", STDERR_FILENO);
+			ft_putstr_fd(MSH_ERR_PFIX ERR_EXIT_ARGC, STDERR_FILENO);
 			ctx->cmd_status = 1;
 		}
 		else
@@ -151,42 +156,53 @@ int	val_var_id(char *var)
 	return (*var == '\0' || *var == '=');
 }
 
+void	export_var(t_shctx *ctx, char *var)
+{
+	t_dlist		*exp_ref;
+	const char	*varval;
+
+	exp_ref = get_export_ref(ctx, var);
+	if (!exp_ref)
+	{
+		ft_dlstadd_back(&ctx->exports, ft_dlstnew(get_var_id(var)));
+		ctx->exports_len++;
+	}
+	else
+	{
+		free(exp_ref->content);
+		exp_ref->content = get_var_id(var);
+	}
+	varval = ft_strchr(var, '=');
+	if (varval)
+		set_var(exp_ref->content, varval, varval + 1);
+}
+
+#define ERR_EXP_INV "`%s\': not a valid identifier\n"
+
 // Need to validate ID, failing an assigment does not prevent others
 int	export_vars(t_shctx *ctx, char **vars)
 {
-	t_dlist		*var_ref;
-	const char	*varval;
+	int			exitval;
 
+	exitval = 0;
 	while (*vars)
 	{
 		if (val_var_id(*vars))
-		{
-			var_ref = get_export_ref(ctx, *vars);
-			if (!var_ref)
-			{
-				ft_dlstadd_back(&ctx->exports, ft_dlstnew(get_var_id(*vars)));
-				ctx->exports_len++;
-			}
-			else
-			{
-				free(var_ref->content);
-				var_ref->content = get_var_id(*vars);
-			}
-			varval = ft_strchr(*vars, '=');
-			if (varval)
-				set_var(var_ref->content, varval, varval + 1);
-		}
+			export_var(ctx, *vars);
 		else
-			ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX "`%s\': not a valid identifier\n", *vars);
+		{
+			ft_dprintf(STDERR_FILENO, MSH_ERR_PFIX ERR_EXP_INV, *vars);
+			exitval = 1;
+		}
 		vars++;
 	}
-	return (0);
+	return (exitval);
 }
 
 int	unset_cmd(t_shctx *ctx, char **var_ids)
 {
-	t_dlist *var_ref;
-	
+	t_dlist	*var_ref;
+
 	while (*var_ids)
 	{
 		var_ref = get_var_ref(ctx, *var_ids);
@@ -206,11 +222,15 @@ int	unset_cmd(t_shctx *ctx, char **var_ids)
 	return (0);
 }
 
+#define EXP_FULL "declare -x %s=\"%s\"\n"
+#define EXP_ID "declare -x %s\n"
+
 int	export_ls_vars(t_shctx *ctx)
 {
 	t_dlist		*lrgst;
 	t_dlist		*dup_exp;
 	t_dlist		*var;
+	char		*varid;
 
 	dup_exp = ft_dlstdup(ctx->exports);
 	while (dup_exp)
@@ -223,10 +243,12 @@ int	export_ls_vars(t_shctx *ctx)
 				lrgst = var;
 			var = var->next;
 		}
+		varid = get_var_id(lrgst->content);
 		if (get_var_val(ctx, lrgst->content))
-			ft_dprintf(STDOUT_FILENO, "declare -x %s=\"%s\"\n", get_var_id(lrgst->content), get_var_val(ctx, lrgst->content));
+			ft_dprintf(STDOUT_FILENO, EXP_FULL, varid, get_var_val(ctx, varid));
 		else
-			ft_dprintf(STDOUT_FILENO, "declare -x %s\n", lrgst->content);
+			ft_dprintf(STDOUT_FILENO, EXP_ID, varid);
+		free(varid);
 		ft_dlstrmone(&dup_exp, lrgst, NULL);
 	}
 	return (0);
