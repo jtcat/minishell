05737200a509bdb764506518318452f17ea3bf9e
jcat <jftcatarino@gmail.com>
2023-11-11 01:35:36 +0000

    fix bonus lexer, remove redudant function

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: leborges <leborges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:29:48 by leborges          #+#    #+#             */
-/*   Updated: 2023/11/11 00:33:37 by jcat             ###   ########.fr       */
+/*   Updated: 2023/11/11 01:32:57 by jcat             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,12 +65,6 @@ bool	lex_op(t_list **token_list, char **cursor)
 	}
 	store_token(token_list, token_start, *cursor - 1, type);
 	return (true);
-}
-
-void	skip(char	**cursor)
-{
-	while (**cursor && !is_wordchar(**cursor) && !is_op(**cursor))
-		(*cursor)++;
 }
 
 t_list	*split_tokens(char *str)
